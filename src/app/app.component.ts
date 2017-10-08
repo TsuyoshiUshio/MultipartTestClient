beb226112f79c4ecd9d46d554adb6a4af208f29b
Tsuyoshi Ushio <ushio@simplearchitect.com>
2017-10-08 12:28:52 +0900

    multipart upload feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Http, Headers, RequestOptions } from '@angular/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  image = '';
+
+  constructor(private http:Http){}
+
+  upload(list: any) {
+    if (list.length <= 0) { return; }
+    let f = list[0];
+    let data = new FormData();
+    console.log("**************** start");
+    console.log(f);
+    console.log("**************** end");
+    data.append('upfile', f, f.name);
+ //   this.http.post('http://localhost:7071/api/HttpTriggerJS', data)
+      this.http.post('https://carreviewver2.azurewebsites.net/api/HttpTriggerMulti', data)
+      .subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+  }
+
 }
